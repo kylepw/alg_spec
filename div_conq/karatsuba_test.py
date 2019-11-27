5f0147c34fd4f70dbe2e05bd62cc1d69e00af3c5
Kyle <kylepw@gmail.com>
2019-11-28 08:59:00 +0900

    Add Karatsuba test

---
+++ div_conq/karatsuba_test.py
@@ -0,0 +1,35 @@
+from karatsuba import kmult
+from random import randint, seed
+import unittest
+
+class TestKaratsubaMult(unittest.TestCase):
+    def test_bad_values(self):
+        pass
+    def test_one_digit(self):
+        seed(1)
+        for _ in range(10):
+            x = randint(1, 9)
+            y = randint(1, 9)
+            self.assertEqual(kmult(x, y), x * y)
+    def test_two_digits(self):
+        seed(1)
+        for _ in range(100):
+            x = randint(10, 99)
+            y = randint(10, 99)
+            self.assertEqual(kmult(x, y), x * y)
+    def test_three_digits(self):
+        seed(1)
+        for _ in range(100):
+            x = randint(100, 999)
+            y = randint(100, 999)
+            self.assertEqual(kmult(x, y), x * y)
+    def test_six_digits(self):
+        seed(1)
+        for _ in range(100):
+            x = randint(100000, 999999)
+            y = randint(100000, 999999)
+            print(x, y)
+            self.assertEqual(kmult(x, y), x * y)
+
+if __name__ == '__main__':
+    unittest.main()
