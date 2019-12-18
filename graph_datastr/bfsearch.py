cdf63ec527f63394913bab8e2a75503401c819de
Kyle <kylepw@gmail.com>
2019-12-18 22:40:32 +0900

    Rename files. Add bfs search

---
+++ graph_datastr/bfsearch.py
@@ -0,0 +1,50 @@
+"""Breadth-first search implementation."""
+from queue import Queue
+import unittest
+
+
+def bfs(graph, value, start=None):
+    """Breadth-first search
+
+        graph (dic): keys are vertices, values are lists of adjacent vertices
+        start: node to start search from
+
+        Return (bool): whether `value` is found or not.
+    """
+    if len(graph) < 1:
+        return False
+    if start is None:
+        start = list(graph.keys())[0]
+    visited = [start]
+    q = Queue()
+    q.put(start)
+    while not q.empty():
+        current = q.get()
+        print(f'visiting {current}')
+        if current == value:
+            return True
+        for a in graph[current]:
+            if a not in visited:
+                visited.append(a)
+                q.put(a)
+    return False
+
+
+
+
+
+class TestBfs(unittest.TestCase):
+    def test_undirected_graph(self):
+        graph = {1: [2, 4], 2: [1, 6], 3: [4], 4: [1, 5, 6], 5: [4], 6: [2, 4, 7], 7: [6]}
+
+        self.assertTrue(bfs(graph, 7))
+        self.assertFalse(bfs(graph, 8))
+    @unittest.SkipTest
+    def test_directed_graph(self):
+        graph = {1: [2, 3], 2: [5], 3: [], 4: [2, 3], 5: [4]}
+
+        self.assertTrue(bfs(graph, 4))
+        self.assertFalse(bfs(graph, 0))
+
+if __name__ == '__main__':
+    unittest.main()
