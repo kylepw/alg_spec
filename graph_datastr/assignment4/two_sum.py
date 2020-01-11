463297d921bf53d07a512763635c72d7d15c6b70
Kyle <kylepw@gmail.com>
2020-01-11 22:59:46 +0900

    Completed assignment 4 two_sum

--- graph_datastr/assignment4/two_sum.py
+++ graph_datastr/assignment4/two_sum.py
@@ -6,19 +6,41 @@
 
 """
 import argparse
+from bisect import bisect_left as bl
+
+
+def find_tsums(nums: set):
+    """Return number of discinct x, y pairs that add up to a number -10000 to 10000."""
+    found = {}
+    for i in range(-10000, 10001):
+        found[i] = False
+
+    for x in nums:
+        for y in [
+            y
+            for y in nums[bl(nums, -10000 - x) : bl(nums, 10000 - x) + 1]
+            if x + y in found and not found[x + y]
+        ]:
+            print(f'{x} + {y} = {x + y}')
+            found[x + y] = True
+
+    return sum([1 for t in found if found[t]])
+
 
 def load_nums(filename):
-    nums = {}
+    """Load sorted list of distinct numbers into list"""
+    nums = set()
     try:
         with open(filename) as f:
-            for i, line in enumerate(f):
-                nums[int(line.strip())] = i
+            for line in f:
+                nums.add(int(line.strip()))
     except IOError:
         print('Failed to read data from {filename}')
         raise
 
-    return nums
+    nums = list(nums)
 
+    return sorted(nums)
 
 
 def get_parser():
@@ -39,8 +61,7 @@ def main():
     args = vars(parser.parse_args())
     filename = args['filename']
 
-    nums = load_nums(filename)
-    print(nums)
+    print(find_tsums(load_nums(filename)))
 
 
 if __name__ == '__main__':
